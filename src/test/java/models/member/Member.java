72947eccf592ac52dddba2c14070b902f1517a24
somi9954 <jolh999@gmail.com>
2023-10-22 21:53:29 +0900

    의존성 추가 설정(pom.xml), 톰캣10 서버 설정 완료

---
+++ src/test/java/models/member/Member.java
@@ -0,0 +1,14 @@
+package models.member;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder @Data
+public class Member {
+    private String userid;
+    private String userPw;
+    private  String conformUserPw;
+    private String userNm;
+    private String email;
+    private boolean agree; //약관 동의
+}
---
+++ src/test/java/tests/JoinServiceTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import models.member.JoinService;
+import models.member.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("회원가입 기능 단위 테스트")
+public class JoinServiceTest {
+
+    private JoinService joinService;
+    @BeforeEach
+    void init() {
+        joinService = new JoinService();
+    }
+
+    private Member getMember() {
+        return Member.builder()
+                .userid("user" + System.currentTimeMillis())
+                .userPw("12345678")
+                .userNm("사용자01")
+                .email("user@test.org")
+                .agree(true)
+                .build();
+    }
+    @Test
+    @DisplayName("회원가입 성공시 예외 발생 하지 않음")
+    void joinSuccess() {
+        assertDoesNotThrow(() -> {
+            joinService.join(getMember());
+        });
+    }
+}
