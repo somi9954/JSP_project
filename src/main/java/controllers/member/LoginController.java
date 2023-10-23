e68ee2ba16dad33c225274731b7895430323a86d
somi9954 <jolh999@gmail.com>
2023-10-24 00:44:08 +0900

    기능통합 - 회원가입

---
+++ src/main/java/controllers/member/LoginController.java
@@ -0,0 +1,38 @@
+package controllers.member;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import models.member.LoginService;
+import models.member.ServiceManager;
+
+import java.io.IOException;
+
+import static commons.ScriptUtil.alertError;
+import static commons.ScriptUtil.go;
+
+@WebServlet("/member/login")
+public class LoginController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/login.jsp");
+        rd.forward(req, resp);
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            LoginService service = ServiceManager.getInstance().loginService();
+            service.login(req);
+
+            go(resp, req.getContextPath() + "/", "parent");
+        } catch (RuntimeException e) {
+             alertError(resp, e);
+        }
+    }
+}
